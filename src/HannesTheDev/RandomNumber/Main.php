2aa6534c04df283354da74c416a7196b67acae66
Hannes <65465954+HannesTheDev@users.noreply.github.com>
2021-05-03 00:16:37 +0200

    Add files via upload

--- src/HannesTheDev/RandomNumber/Main.php
+++ src/HannesTheDev/RandomNumber/Main.php
@@ -1,1 +1,41 @@
+<?php
 
+namespace HannesTheDev\RandomNumber;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase
+{
+    public $number;
+    public $min;
+    public $max;
+
+
+    public function onEnable()
+    {
+        foreach (['config.yml'] as $ressources) {
+            $this->saveResource($ressources);
+        }
+        $config = new Config($this->getDataFolder() . 'config.yml');
+        $this->min = $config->get("min");
+        $this->max = $config->get("max");
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool
+    {
+        switch ($cmd->getName()) {
+            case "randomnumber":
+                if ($sender->hasPermission("randomnumber.randomnumber.cmd")) {
+                    if (!empty($this->things)) {
+                        $random = rand($this->min, $this->max);
+                        $sender->sendMessage("§3Your random number is: §e" . $random);
+                    }
+                }
+                break;
+        }
+        return true;
+    }
+}
